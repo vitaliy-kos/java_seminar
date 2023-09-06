ea4519971ce11cf37e1e19ba272326ac30fb865a
Vitaliy <kv1993@me.com>
2023-09-06 14:47:37 +0300

    Updated task01

--- task01/program.java
+++ task01/program.java
@@ -15,28 +15,35 @@ public class program {
 
         public static void sort(int[] mas) {
 
-            for (int i = 0; i < mas.length; i++) {
+            int min = 0;
+            String res = "";
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("y-MM-dd H:mm");
 
-                for (int j = 0; j < mas.length; j++) {
-                    if (mas[i] < mas[j]){
+            if (mas[0] == 9 && mas.length > 5) {
+                min = 2;
+            }
+            
+            for (int i = 0; i < mas.length - min; i++) {
+                for (int j = 0; j < mas.length - i - 1; j++) {
+                    if (mas[j] > mas[j + 1]){
                         int temp = mas[j];
-                        mas[j] = mas[i];
-                        mas[i] = temp;
+                        mas[j] = mas[j + 1];
+                        mas[j + 1] = temp;
                     }
                 }
 
-                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("y-MM-dd H:mm");
                 String date_str = simpleDateFormat.format(new Date());
+                res = res + date_str + " " + Arrays.toString(mas) + "\n";
 
-                try {
-                    FileWriter writer = new FileWriter("log.txt", true);
-                    BufferedWriter bufferWriter = new BufferedWriter(writer);
-                    bufferWriter.write(date_str + " " + Arrays.toString(mas) + "\n");
-                    bufferWriter.close();
-                } catch (IOException e) {
-                    System.out.println(e);
-                }
+            }
 
+            try {
+                FileWriter writer = new FileWriter("log.txt", false);
+                BufferedWriter bufferWriter = new BufferedWriter(writer);
+                bufferWriter.write(res);
+                bufferWriter.close();
+            } catch (IOException e) {
+                System.out.println(e);
             }
         
         }
@@ -50,6 +57,8 @@ public class program {
         if (args.length == 0) {
             // При отправке кода на Выполнение, вы можете варьировать эти параметры
             arr = new int[]{9, 4, 8, 3, 1};
+            // arr = new int[]{9, 3, 4, 8, 2, 5, 7, 1, 6, 10};
+            // arr = new int[]{10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
         }
         else{
             arr = Arrays.stream(args[0].split(", "))
